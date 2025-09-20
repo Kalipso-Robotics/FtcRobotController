738c2b78c428e7cc10251d64e8e7f78fe24fc5f7
Alan Huang <alan.wenhao.huang@gmail.com>
2025-09-20 11:48:49 -0700

    Goal detection with logs

--- TeamCode/src/main/java/com/kalipsorobotics/test/cameraVision/GoalDetectionTest.java
+++ TeamCode/src/main/java/com/kalipsorobotics/test/cameraVision/GoalDetectionTest.java
@@ -6,7 +6,9 @@ import com.kalipsorobotics.cameraVision.GoalDetection;
 import com.kalipsorobotics.cameraVision.ObiliskDetection;
 import com.kalipsorobotics.utilities.SharedData;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp(name="Goal Detection Test")
 public class GoalDetectionTest extends LinearOpMode {
     private GoalDetection goalDetection;
     @Override
@@ -14,8 +16,10 @@ public class GoalDetectionTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         goalDetection = new GoalDetection();
         while (opModeIsActive()) {
-            Log.d("Goal detection", "" + goalDetection.getGoalColor(goalDetection.getGoalId()) + " " + "distance: " +
-                    goalDetection.getDistanceToGoal() + "heading error: " + goalDetection.getHeadingError(SharedData.getOdometryPosition().getTheta()));
+            Log.d("Goal detection",
+                    "Alliance Color: " + goalDetection.getGoalColor(goalDetection.getGoalId()) +
+                    " distance: " + goalDetection.getDistanceToGoal() +
+                    " heading error: " + goalDetection.getHeadingError(SharedData.getOdometryPosition().getTheta()));
         }
     }
 }
