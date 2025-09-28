d2882f90ba3971e2e62c5748d72b4b6d5801a111
Alan Huang <alan.wenhao.huang@gmail.com>
2025-09-28 13:43:37 -0700

    executer services for odometry

--- TeamCode/src/main/java/com/kalipsorobotics/test/cameraVision/AutoDriveWithMotifDetection.java
+++ TeamCode/src/main/java/com/kalipsorobotics/test/cameraVision/AutoDriveWithMotifDetection.java
@@ -2,25 +2,42 @@ package com.kalipsorobotics.test.cameraVision;
 
 import android.util.Log;
 
+import com.kalipsorobotics.cameraVision.ObiliskDetection;
+import com.kalipsorobotics.localization.Odometry;
 import com.kalipsorobotics.modules.DriveTrain;
+import com.kalipsorobotics.modules.GoBildaOdoModule;
+import com.kalipsorobotics.modules.IMUModule;
 import com.kalipsorobotics.navigation.PurePursuitAction;
 import com.kalipsorobotics.utilities.KMotifDetectionAction;
 import com.kalipsorobotics.utilities.OpModeUtilities;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Autonomous
 public class AutoDriveWithMotifDetection extends LinearOpMode {
     KMotifDetectionAction motifDetection;
+    ObiliskDetection obiliskDetection;
     @Override
     public void runOpMode() throws InterruptedException {
         OpModeUtilities opModeUtilities = new OpModeUtilities(hardwareMap, this, telemetry);
         DriveTrain.setInstanceNull();
         DriveTrain driveTrain = DriveTrain.getInstance(opModeUtilities);
-
+        obiliskDetection = new ObiliskDetection();
+        obiliskDetection.init(hardwareMap);
+        motifDetection = new KMotifDetectionAction(obiliskDetection);
+        IMUModule imuModule = IMUModule.getInstance(opModeUtilities);
+        GoBildaOdoModule goBildaOdoModule = GoBildaOdoModule.getInstance(opModeUtilities);
+        Odometry odometry = Odometry.getInstance(opModeUtilities, driveTrain, imuModule, goBildaOdoModule);
         PurePursuitAction moveForward = new PurePursuitAction(driveTrain);
-        moveForward.addPoint(500, 0, 0);
+        moveForward.addPoint(-500, 0, 0);
         moveForward.setMaxTimeOutMS(8000);
+
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        OpModeUtilities.runOdometryExecutorService(executorService, odometry);
 
         waitForStart();
         while(opModeIsActive()) {
@@ -28,6 +45,10 @@ public class AutoDriveWithMotifDetection extends LinearOpMode {
             motifDetection.updateCheckDone();
             Log.d("Motif Detection Auto", "Motif Pattern: " + motifDetection.getMotifPattern());
             telemetry.addLine("" + motifDetection.getMotifPattern());
+            if (motifDetection.getIsDone()) {
+                break;
+            }
         }
+        OpModeUtilities.shutdownExecutorService(executorService);
     }
 }
