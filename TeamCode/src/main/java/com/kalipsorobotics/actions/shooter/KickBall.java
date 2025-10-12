8878b852e2ef331d16e68335119eebe48c58705d
Ethan2502 <ethan.mt.vu@gmail.com>
2025-10-12 12:44:15 -0700

    kicker position

--- TeamCode/src/main/java/com/kalipsorobotics/actions/shooter/KickBall.java
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/shooter/KickBall.java
@@ -18,7 +18,7 @@ public class KickBall extends KActionSet {
         WaitAction waitAction = new WaitAction(500);
         waitAction.setName("waitAction");
         this.addAction(waitAction);
-        KServoAutoAction staticRight = new KServoAutoAction(shooter.getKickerRight(), 0.85);
+        KServoAutoAction staticRight = new KServoAutoAction(shooter.getKickerRight(), 0.8);
         staticRight.setName("staticRight");
         this.addAction(staticRight);
         staticRight.setDependentActions(kickRight, waitAction);
