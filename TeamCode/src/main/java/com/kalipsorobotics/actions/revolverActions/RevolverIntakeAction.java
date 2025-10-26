ab19df8dcef4737377b79397b477235f7a677ddc
k8liang <k8lcool@gmail.com>
2025-10-25 17:07:53 -0700

    tried to do delayed turning for revolver intake but not working commented out

--- TeamCode/src/main/java/com/kalipsorobotics/actions/revolverActions/RevolverIntakeAction.java
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/revolverActions/RevolverIntakeAction.java
@@ -7,6 +7,7 @@ import com.kalipsorobotics.modules.Revolver;
 import com.kalipsorobotics.modules.TripleColorSensor;
 import com.kalipsorobotics.utilities.KLog;
 import com.kalipsorobotics.utilities.KServo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import java.util.Arrays;
 
@@ -18,57 +19,44 @@ public class RevolverIntakeAction extends Action {
 
     private final DetectColorsAction detectColorsAction;
 
+//    ElapsedTime timer = new ElapsedTime();
+
 
     public RevolverIntakeAction(Revolver revolver, TripleColorSensor colorSensors) {
         this.revolver = revolver;
         this.revolverServo = revolver.getRevolverServo();
         this.dependentActions.add(new DoneStateAction());
         this.detectColorsAction = new DetectColorsAction(colorSensors, revolver.getOpModeUtilities());
+//        this.timer = new ElapsedTime();
     }
 
 
     @Override
     protected void update() {
 
-        if (revolverServo.isDone() & hasStarted) {
-            detectColorsAction.setIsDone(false);
-            detectColorsAction.updateCheckDone();
-        }
-
-        if (!hasStarted) {
+        if (!hasStarted || (revolverServo.isDone() & hasStarted)) {
             hasStarted = true;
             detectColorsAction.setIsDone(false);
             detectColorsAction.updateCheckDone();
+//            timer.reset();
         }
 
         if (detectColorsAction.isFull()) {
             KLog.d("teleoprevolver", "done revolver intake");
             isDone = true;
-        } else if (revolverServo.isDone() && !(detectColorsAction.getFrontColor() == MotifColor.NONE)){
+        } else if (revolverServo.isDone() && !(detectColorsAction.getFrontColor() == MotifColor.NONE)){ // && timer.milliseconds()>200
             MotifColor[] motifColors = {detectColorsAction.getFrontColor(), detectColorsAction.getBrightColor(), detectColorsAction.getBLeftColor()};
             int emptySlotSensorIndex = Arrays.asList(motifColors).indexOf(MotifColor.NONE); //todo make it find the closest one
             KLog.d("teleoprevolver", "sensor list " + Arrays.asList(motifColors));
             int currentTrayIndex = revolver.getCurrentRevolverServoIndex();
             KLog.d("teleoprevolver", "empty sensor index " + emptySlotSensorIndex);
             KLog.d("teleoprevolver", "current tray index " + currentTrayIndex);
-//            int indDiff = currentTrayIndex - emptySlotSensorIndex;
 
             int moveTo = (currentTrayIndex + emptySlotSensorIndex) % 3;
             KLog.d("teleoprevolver", "move to " + moveTo);
 
             revolver.moveToIndex(moveTo);
         }
-
-        /* todo test:
-            nothing in tray
-            1 in tray in 3
-            1 in tray in 2
-            1 in tray in 1
-            2 in tray in 3,2
-            2 in tray in 2,1
-            2 in tray in 3,1
-            3 in tray
-        */
 
     }
 }
