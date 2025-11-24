b29d630493bd2f3b6a3606cc84cd2c9391d373e9
Darren Chan <2darrenchan@gmail.com>
2025-11-23 22:47:05 -0800

    good blue depot, battery dependant, maybe not as consistant as could be

--- TeamCode/src/main/java/com/kalipsorobotics/modules/shooter/ShooterInterpolationDataLookup.java
+++ TeamCode/src/main/java/com/kalipsorobotics/modules/shooter/ShooterInterpolationDataLookup.java
@@ -29,26 +29,7 @@ public class ShooterInterpolationDataLookup implements IShooterPredictor {
     // Static data from ShooterTestedManualData.csv
     // Format: DistanceMM, RPS, Hood Position Relative
     private static final ArrayList<DataPoint> DATA_POINTS = new ArrayList<DataPoint>() {{
-//        add(new DataPoint(540, 34, 0.1));
-//        add(new DataPoint(682, 34, 0.31));
-//        add(new DataPoint(913, 35, 0.38));
-//        add(new DataPoint(1108, 36, 0.55));
-//        add(new DataPoint(1323, 37, 0.55));
-//        add(new DataPoint(1580, 37.5, 0.55));
-//        add(new DataPoint(1698, 38, 0.55));
-//        add(new DataPoint(1808, 38, 0.55));
-//        add(new DataPoint(1955, 39, 0.55));
-//        add(new DataPoint(2057, 39, 0.55));
-//        add(new DataPoint(2179, 40, 0.55));
-//        add(new DataPoint(2330, 41, 0.55));
-//        add(new DataPoint(2516, 42, 0.55));
-//        add(new DataPoint(2639, 42, 0.55));
-//        add(new DataPoint(2825, 42, 0.55));
-//        add(new DataPoint(2952, 43.5, 0.55));
-//        add(new DataPoint(3076, 43.5, 0.55));
-//        add(new DataPoint(3304, 44, 0.55));
-//        add(new DataPoint(3389, 44, 0.55));
-//        add(new DataPoint(3547, 44, 0.55));
+        //in  ShooterInterpolationConfig
     }};
 
     /**
