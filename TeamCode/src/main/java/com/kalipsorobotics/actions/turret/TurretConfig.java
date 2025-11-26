97a8e0cda5b879fddbfb211ecbf89f3722d5de53
Darren Chan <2darrenchan@gmail.com>
2025-11-25 20:53:14 -0800

    merge

--- TeamCode/src/main/java/com/kalipsorobotics/actions/turret/TurretConfig.java
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/turret/TurretConfig.java
@@ -8,5 +8,7 @@ public class TurretConfig { //
     public static double X_INIT_SETUP_MM = 3250 + 150; //121 inches
 
     public static double Y_INIT_SETUP_MM = 1050 + 340; //46inches 1000
-
+    public static double kP = 0.002;    // proportional
+    public static double kI = 0.000005 ;  // tiny integral
+    public static double kD = 0.00004;
 }
