1a8e8faa39c29593e9e6f8383ccffdc42b5da131
Darren Chan <2darrenchan@gmail.com>
2025-12-05 17:32:26 -0800

    comp ready again...

--- TeamCode/src/main/java/com/kalipsorobotics/actions/turret/TurretConfig.java
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/turret/TurretConfig.java
@@ -8,9 +8,9 @@ public class TurretConfig { //
     public static double X_INIT_SETUP_MM = 3357; //3400
     public static double Y_INIT_SETUP_MM = 1350; // 1400
     public static double TICKS_INIT_OFFSET = 0;
-    public static double kP = 0.0021;
+    public static double kP = 0.0021; //0.0021
     public static double kI = 0.00003; //0.000030
-    public static double kD = 0.000165; //0.000090
-    public static double kS = 0.15; // static base
+    public static double kD = 0.000165; //
+    public static double kS = 0.15; // static base // was 0.15
 
 }
