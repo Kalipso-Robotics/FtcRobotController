c7e1325e29f1cbe64da41aa07be96a94a4292993
Ethan2502 <ethan.mt.vu@gmail.com>
2025-12-19 20:39:40 -0800

    retune turret

--- TeamCode/src/main/java/com/kalipsorobotics/actions/turret/TurretConfig.java
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/turret/TurretConfig.java
@@ -8,9 +8,9 @@ public class TurretConfig { //
     public static double X_INIT_SETUP_MM = 3357; //3400
     public static double Y_INIT_SETUP_MM = 1350; // 1400
     public static double TICKS_INIT_OFFSET = 0;
-    public static double kP = 0.0021; //0.0021
-    public static double kI = 0.00003; //0.000030
-    public static double kD = 0.000165; //
+    public static double kP = 0.0226; //0.0021
+    public static double kI = 0; //0.00003; //0.000030
+    public static double kD = 0; // 0.000165; //
     public static double kS = 0.1; // static base // was 0.15
 
 }
