3381ba13ec4e8f53df0753bf7a993b248fe584f2
Darren Chan <2darrenchan@gmail.com>
2025-12-21 15:01:01 -0800

    tuned turret PID

--- TeamCode/src/main/java/com/kalipsorobotics/actions/drivetrain/ReleaseBraking.java
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/drivetrain/ReleaseBraking.java
@@ -6,21 +6,20 @@ import com.kalipsorobotics.actions.actionUtilities.KServoAutoAction;
 import com.kalipsorobotics.modules.DriveBrake;
 import com.kalipsorobotics.utilities.KLog;
 
-public class ReleaseBraking extends KActionSet {
+public class ReleaseBraking extends Action {
 
     public static final double RELEASE_BRAKE_RIGHT_POS = 0.7095;
     public static final double RELEASE_BRAKE_LEFT_POS = 0.8199;
 
+    private DriveBrake driveBrake;
+
     public ReleaseBraking(DriveBrake driveBrake) {
-
-        KServoAutoAction brakeRight = new KServoAutoAction(driveBrake.getBrakeRight(), RELEASE_BRAKE_RIGHT_POS);
-        brakeRight.setName("brakeRight");
-        this.addAction(brakeRight);
-
-        KServoAutoAction brakeLeft = new KServoAutoAction(driveBrake.getBrakeLeft(), RELEASE_BRAKE_LEFT_POS);
-        brakeLeft.setName("brakeLeft");
-        this.addAction(brakeLeft);
-        KLog.d("Braking", "Brakes released");
+        this.driveBrake = driveBrake;
     }
 
+    @Override
+    protected void update() {
+        driveBrake.getBrakeLeft().setTargetPosition(RELEASE_BRAKE_LEFT_POS);
+        driveBrake.getBrakeRight().setTargetPosition(RELEASE_BRAKE_RIGHT_POS);
+    }
 }
