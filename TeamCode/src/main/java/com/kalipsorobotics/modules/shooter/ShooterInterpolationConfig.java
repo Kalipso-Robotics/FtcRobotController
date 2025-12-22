6878b01befd5aa1eac3fd551391d32c00d2f82f3
Ethan2502 <ethan.mt.vu@gmail.com>
2025-12-21 17:00:11 -0800

    Max hood good

--- TeamCode/src/main/java/com/kalipsorobotics/modules/shooter/ShooterInterpolationConfig.java
+++ TeamCode/src/main/java/com/kalipsorobotics/modules/shooter/ShooterInterpolationConfig.java
@@ -6,44 +6,46 @@ import java.util.ArrayList;
 
 @Config
 public class ShooterInterpolationConfig {
+    public final static double MAX_HOOD = 0.71;
+    public static final double NEAR_SHOOT_RPS = 32.86;
     public static double[] shooterData = {
-            602.23, 28.57, 0.25,
-            724, 32, 0.25,
-            918, 33.4, 0.35,
-            1126, 32, 0.45,
-            1143.96, 31.5, 0.8, // FIRST SHOOT NEAR,
-            1251.09, 32.14, 0.8,
-            1290.48, 31.43, 0.8,
-            1321.83, 30.71, 0.8,
-            1341.74, 32.14, 0.8,
-            1411.42, 32.86, 0.8,
-            1496.56, 30, 0.8,
-            1496.59, 32.86, 0.8,
-            1526.71, 34.29, 0.8,
-            1595.31, 32.86, 0.8, // SECOND SHOOT NEAR,
-            1700.15, 32.14, 0.8,
-            1813.77, 32.86, 0.8,
-            1931.24, 34.29, 0.8,
-            2070.17, 35, 0.8,
-            2186.13, 37.86, 0.8,
-            2186.2, 37.86, 0.8,
-            2328.5, 38.57, 0.8,
-            2328.51, 40.71, 0.8,
-            2446.11, 41, 0.8,
-            2590.71, 42.14, 0.8,
-            2729.31, 44.29, 0.8,
-            2865.43, 43.29, 0.8,
-            3001.9, 45, 0.8,
-            3138.04, 44.5, 0.8,
-            3283.8, 46, 0.8, // 2 + 3 SHOOT FAR,
-            3441.86, 46.6, 0.8, // FIRST SHOOT FAR,
+            602.23, 28.57, 0,
+            724, 32, 0,
+            918, 33.4, 0.1,
+            1126, 32, 0.2,
+            1143.96, 31.5, MAX_HOOD, // FIRST SHOOT NEAR,
+            1251.09, 32.14, MAX_HOOD,
+            1290.48, 31.43, MAX_HOOD,
+            1321.83, 30.71, MAX_HOOD,
+            1341.74, 32.14, MAX_HOOD,
+            1411.42, 32.86, MAX_HOOD,
+            1496.56, 30, MAX_HOOD,
+            1496.59, 32.86, MAX_HOOD,
+            1526.71, 34.29, MAX_HOOD,
+            1595.31, NEAR_SHOOT_RPS, MAX_HOOD, // SECOND SHOOT NEAR,
+            1700.15, 32.14, MAX_HOOD,
+            1813.77, 32.86, MAX_HOOD,
+            1931.24, 34.29, MAX_HOOD,
+            2070.17, 35, MAX_HOOD,
+            2186.13, 37.86, MAX_HOOD,
+            2186.2, 37.86, MAX_HOOD,
+            2328.5, 38.57, MAX_HOOD,
+            2328.51, 40.71, MAX_HOOD,
+            2446.11, 41, MAX_HOOD,
+            2590.71, 42.14, MAX_HOOD,
+            2729.31, 44.29, MAX_HOOD,
+            2865.43, 43.29, MAX_HOOD,
+            3001.9, 45, MAX_HOOD,
+            3138.04, 44.5, MAX_HOOD,
+            3283.8, 46, MAX_HOOD, // 2 + 3 SHOOT FAR,
+            3441.86, 46.6, MAX_HOOD, // FIRST SHOOT FAR,
     };
 
     public static double[] getMaxValue() {
-        return new double[] {46.6, 0.8};
+        return new double[] {shooterData[shooterData.length - 2], shooterData[shooterData.length - 1]};
     }
     public static double[] getNearValue() {
-        return new double[] {32.86, 0.8};
+        return new double[] {NEAR_SHOOT_RPS, MAX_HOOD};
     }
 
 }
