a3f984d2fed00263083946558e0747f4242fc1b1
Darren Chan <2darrenchan@gmail.com>
2026-01-17 15:12:56 -0800

    depot 18 balls

--- TeamCode/src/main/java/com/kalipsorobotics/decode/configs/ShooterConfig.java
+++ TeamCode/src/main/java/com/kalipsorobotics/decode/configs/ShooterConfig.java
@@ -1,8 +1,11 @@
 package com.kalipsorobotics.decode.configs;
 
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
 public class ShooterConfig {
-    public static double timeToStabilize = 100;
-    public static double kp = 0.012;
+    public static double timeToStabilize = 60;
+    public static double kp = 0.015;
     public static double ki = 0.001;
     public static double kd = 0.012;
     public static double kf = 0.01205432614;
--- TeamCode/src/main/java/com/kalipsorobotics/decode/configs/ShooterInterpolationConfig.java
+++ TeamCode/src/main/java/com/kalipsorobotics/decode/configs/ShooterInterpolationConfig.java
@@ -23,10 +23,10 @@ public class ShooterInterpolationConfig {
             2400, 41.9, MAX_HOOD,
             2570, 44.9, MAX_HOOD,
             FAR_DISTANCE, 45.5, MAX_HOOD, // TIP AREA DON'T TOUCH, far shoot
-            2965, 47.7, MAX_HOOD, // TIP AREA !!! very good like 80% accuracy DON'T TOUCH. far shoot
-            3095, 48.0, MAX_HOOD, // FAR SHOOT, first shoot ( 0 , 0 )
-            3300, 49.3, MAX_HOOD,
-            3500, 50.2, MAX_HOOD,
+            2965, 47.2, MAX_HOOD, // TIP AREA !!! very good like 80% accuracy DON'T TOUCH. far shoot
+            3095, 47.5, MAX_HOOD, // FAR SHOOT, first shoot ( 0 , 0 )
+            3300, 48.8, MAX_HOOD,
+            3500, 49.7, MAX_HOOD,
     };
 
 
