34f80986671253d1a28eb9fb04572fdcb60fc90b
Vu <ethan.mt.vu@gmail.com>
2026-02-10 11:08:23 -0800

    tune shoot time and stopper pos

--- TeamCode/src/main/java/com/kalipsorobotics/actions/intake/IntakeConfig.java
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/intake/IntakeConfig.java
@@ -2,7 +2,7 @@ package com.kalipsorobotics.actions.intake;
 
 public class IntakeConfig {
 
-    public static double shootTimeMS = 380;
+    public static double shootTimeMS = 450;
     public static double intakeBallTimeMS = 7500;
     public static double intakePower = 1;
     public static double intakeFromRampTime = 2300;
