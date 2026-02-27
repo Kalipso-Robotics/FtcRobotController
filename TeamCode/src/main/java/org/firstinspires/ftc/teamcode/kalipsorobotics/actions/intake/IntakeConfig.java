211922fe9fe960f480e01b3aeedaed611860765f
Alan Huang <alan.wenhao.huang@gmail.com>
2026-02-26 20:54:20 -0800

    intake config stopper tune

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kalipsorobotics/actions/intake/IntakeConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kalipsorobotics/actions/intake/IntakeConfig.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.kalipsorobotics.actions.intake;
 
 public class IntakeConfig {
 
-    public static double shootTimeMS = 450;
+    public static double shootTimeMS = 500;
     public static double intakeBallTimeMS = 5000;
     public static double intakePower = 1;
     public static double intakeFromRampTime = 2300;
