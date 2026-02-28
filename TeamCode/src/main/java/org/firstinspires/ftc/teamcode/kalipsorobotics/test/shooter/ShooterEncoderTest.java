f06b0dd37996f28b29b2c1022f19d4e8773179fc
Vu <ethan.mt.vu@gmail.com>
2026-02-28 10:56:04 -0800

    tune depot shoot timeout

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kalipsorobotics/test/shooter/ShooterEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kalipsorobotics/test/shooter/ShooterEncoderTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.kalipsorobotics.modules.Turret;
 import org.firstinspires.ftc.teamcode.kalipsorobotics.modules.shooter.Shooter;
 import org.firstinspires.ftc.teamcode.kalipsorobotics.utilities.OpModeUtilities;
 
@@ -14,6 +15,7 @@ public class ShooterEncoderTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         OpModeUtilities opModeUtilities = new OpModeUtilities(hardwareMap, this, telemetry);
         Shooter shooter = new Shooter(opModeUtilities);
+        Turret turret = Turret.getInstance(opModeUtilities);
 
         waitForStart();
         while (opModeIsActive()) {
@@ -30,9 +32,18 @@ public class ShooterEncoderTest extends LinearOpMode {
                 shooter.getShooter2().setPower(0);
             }
 
+            if (gamepad1.a) {
+                turret.turretMotor.setPower(0.2);
+            } else if (gamepad1.b) {
+                turret.turretMotor.setPower(-0.2);
+            } else {
+                turret.turretMotor.setPower(0);
+            }
+
 
             telemetry.addData("shooter1", shooter.getShooter1().getCurrentPosition());
             telemetry.addData("shooter2", shooter.getShooter2().getCurrentPosition());
+            telemetry.addData("turret", turret.getTurretMotor().getCurrentPosition());
 
             telemetry.update();
 
