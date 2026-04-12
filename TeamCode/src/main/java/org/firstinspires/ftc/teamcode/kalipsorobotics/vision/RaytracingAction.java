1fae5b8a0dca8a5814294145a0a47232261ea7be
Darren Chan <2darrenchan@gmail.com>
2026-04-12 12:59:04 -0700

    added Intrinsics to raytracing but will move soon

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kalipsorobotics/vision/RaytracingAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kalipsorobotics/vision/RaytracingAction.java
@@ -1,24 +1,38 @@
 package org.firstinspires.ftc.teamcode.kalipsorobotics.vision;
 
+import org.firstinspires.ftc.teamcode.kalipsorobotics.actions.actionUtilities.Action;
 import org.firstinspires.ftc.teamcode.kalipsorobotics.math.Point;
 import org.firstinspires.ftc.teamcode.kalipsorobotics.math.Vector3d;
+/*
+camera_intrinsics:
+Image size : 1280 x 800
+fx=888.2839 fy=887.1260
+cx=700.0372 cy=354.0664
+k1=0.045011 k2=-0.059862 k3=0.000330
+p1=0.001499 p2=0.005590
+*/
 
-public class Raytracing {
+public class RaytracingAction extends Action {
     // CONSTANTS (FIND THESE)
-    public static double FOCAL_LENGTH = 640;
-    public static double IMAGE_WIDTH = 640;
-    public static int IMAGE_HEIGHT = 480;
+    public static double FOCAL_LENGTH = 888; // avg of fx and fy
+    public static double CX = 700.0372;
+    public static double CY = 354.0664;
     public static double MOUNT_ANGLE = Math.toRadians(0); // angle DOWN to the floor
-
     Vector3d cameraOffset = new Vector3d(0, 0, 0); // sideways, height, forward
 
+    @Override
+    protected void update() {
+
+    }
+
+
     /**
-     * converts a 2D pixel coordinate to a 2D world coordinate on the floor
+     * converts a 2D pixel coordinate (blob) to a 2D world coordinate on the floor
      */
     public Point calculateWorldPos(double pixelX, double pixelY) { // these pixels should be the bottom center of the blob
         // normalize center to middle of image not corner
-        double norm_x = pixelX - (IMAGE_WIDTH / 2.0);
-        double norm_y = pixelY - (IMAGE_HEIGHT / 2.0);
+        double norm_x = pixelX - CX;
+        double norm_y = pixelY - CY;
 
         double x_direction = norm_x / FOCAL_LENGTH;
         double y_direction = norm_y / FOCAL_LENGTH;
@@ -46,6 +60,5 @@ public class Raytracing {
 
         return new Point(xFromRobotCenter, zFromRobotCenter);
     }
-
 }
 
