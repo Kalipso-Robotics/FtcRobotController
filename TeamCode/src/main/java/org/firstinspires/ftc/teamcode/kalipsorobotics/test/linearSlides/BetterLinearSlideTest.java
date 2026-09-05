2f0199d4e6c9e4b6c88676e22c663db9792150a0
DarrenChan99 <2darrenchan@gmail.com>
2026-09-05 15:10:30 -0700

    better linear slide test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kalipsorobotics/test/linearSlides/BetterLinearSlideTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kalipsorobotics/test/linearSlides/BetterLinearSlideTest.java
@@ -9,6 +9,7 @@ import org.firstinspires.ftc.teamcode.kalipsorobotics.math.MathFunctions;
 
 /**
  * Joystick-driven power test for up to 2 slide motors.
+ * Left stick drives both motors at full (ceiling) speed in whichever direction it's pushed.
  * Right/left bumper tunes the shared power ceiling down/up from the default [-1, 1] range.
  */
 @TeleOp
@@ -24,13 +25,13 @@ public class BetterLinearSlideTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        DcMotor slideMotor1 = hardwareMap.dcMotor.get("PLACEHOLDER1");
-        slideMotor1.setDirection(DcMotorSimple.Direction.FORWARD);
+        DcMotor slideMotor1 = hardwareMap.dcMotor.get("motor0");
+        slideMotor1.setDirection(DcMotorSimple.Direction.REVERSE);
         slideMotor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         slideMotor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        DcMotor slideMotor2 = hardwareMap.dcMotor.get("PLACEHOLDER2");
-        slideMotor2.setDirection(DcMotorSimple.Direction.REVERSE);
+        DcMotor slideMotor2 = hardwareMap.dcMotor.get("motor1");
+        slideMotor2.setDirection(DcMotorSimple.Direction.FORWARD);
         slideMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         slideMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
@@ -38,8 +39,9 @@ public class BetterLinearSlideTest extends LinearOpMode {
         while (opModeIsActive()) {
             maxPower = updateMaxPower(maxPower);
 
-            driveSlide(slideMotor1, -gamepad1.left_stick_y, maxPower);
-            driveSlide(slideMotor2, -gamepad1.right_stick_y, maxPower);
+            double drivePower = Math.signum(-gamepad1.left_stick_y) * maxPower;
+            slideMotor1.setPower(drivePower);
+            slideMotor2.setPower(drivePower);
 
             telemetry.addData("maxPower", maxPower);
             telemetry.addData("motor1 power", slideMotor1.getPower());
@@ -59,9 +61,5 @@ public class BetterLinearSlideTest extends LinearOpMode {
         prevRightBumper = gamepad1.right_bumper;
         prevLeftBumper = gamepad1.left_bumper;
         return MathFunctions.clamp(newMaxPower, MIN_MAX_POWER, MAX_MAX_POWER);
-    }
-
-    private void driveSlide(DcMotor motor, double stickInput, double maxPower) {
-        motor.setPower(MathFunctions.clamp(stickInput, -maxPower, maxPower));
     }
 }
